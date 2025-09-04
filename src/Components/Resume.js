3b5f3952d797c423dc0bf71fa6749b8df9af10e2
Dre Graham <dre.graham@auctane.com>
2025-09-04 23:39:04 +0000

    toLowerCase replacements

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { asList, asStr, lower } from "../utils/safe"; // <-- add this import
 
 class Resume extends Component {
   getRandomColor() {
@@ -13,87 +14,84 @@ class Resume extends Component {
   render() {
     if (!this.props.data) return null;
 
-    const skillmessage = this.props.data.skillmessage;
-    const education = this.props.data.education.map(function (education) {
-      return (
-        <div key={education.school}>
-          <h3>{education.school}</h3>
-          <p className="info">
-            {education.degree} <span>&bull;</span>
-            <em className="date">{education.graduated}</em>
-          </p>
-          <p>{education.description}</p>
-        </div>
-      );
-    });
+    const skillmessage = asStr(this.props.data.skillmessage);
 
-    const work = this.props.data.work.map(function (work) {
-      return (
-        <div key={work.company}>
-          <h3>{work.company}</h3>
-          <p className="info">
-            {work.title}
-            <span>&bull;</span> <em className="date">{work.years}</em>
-          </p>
-          <p>{work.description}</p>
-        </div>
-      );
-    });
+    const education = asList(this.props.data.education).map((education) => (
+      <div key={asStr(education?.school)}>
+        <h3>{asStr(education?.school)}</h3>
+        <p className="info">
+          {asStr(education?.degree)} <span>&bull;</span>
+          <em className="date">{asStr(education?.graduated)}</em>
+        </p>
+        <p>{asStr(education?.description)}</p>
+      </div>
+    ));
 
-    const skills = this.props.data.skills.map((skills) => {
+    const work = asList(this.props.data.work).map((work) => (
+      <div key={asStr(work?.company)}>
+        <h3>{asStr(work?.company)}</h3>
+        <p className="info">
+          {asStr(work?.title)}
+          <span>&bull;</span> <em className="date">{asStr(work?.years)}</em>
+        </p>
+        <p>{asStr(work?.description)}</p>
+      </div>
+    ));
+
+    const skills = asList(this.props.data.skills).map((skills) => {
       const backgroundColor = this.getRandomColor();
-      const className = "bar-expand " + skills.name.toLowerCase();
-      const width = skills.level;
+      const className = "bar-expand " + lower(skills?.name);
+      const width = asStr(skills?.level);
 
       return (
-        <li key={skills.name}>
+        <li key={asStr(skills?.name)}>
           <span style={{ width, backgroundColor }} className={className}></span>
-          <em>{skills.name}</em>
+          <em>{asStr(skills?.name)}</em>
         </li>
       );
     });
 
     return (
       <section id="resume">
-          <div className="row education">
-            <div className="three columns header-col">
-              <h1>
-                <span>Education</span>
-              </h1>
-            </div>
-
-            <div className="nine columns main-col">
-              <div className="row item">
-                <div className="twelve columns">{education}</div>
-              </div>
-            </div>
+        <div className="row education">
+          <div className="three columns header-col">
+            <h1>
+              <span>Education</span>
+            </h1>
           </div>
 
-          <div className="row work">
-            <div className="three columns header-col">
-              <h1>
-                <span>Work</span>
-              </h1>
-            </div>
-
-            <div className="nine columns main-col">{work}</div>
-          </div>
-
-          <div className="row skill">
-            <div className="three columns header-col">
-              <h1>
-                <span>Skills</span>
-              </h1>
-            </div>
-
-            <div className="nine columns main-col">
-              <p>{skillmessage}</p>
-
-              <div className="bars">
-                <ul className="skills">{skills}</ul>
-              </div>
+          <div className="nine columns main-col">
+            <div className="row item">
+              <div className="twelve columns">{education}</div>
             </div>
           </div>
+        </div>
+
+        <div className="row work">
+          <div className="three columns header-col">
+            <h1>
+              <span>Work</span>
+            </h1>
+          </div>
+
+          <div className="nine columns main-col">{work}</div>
+        </div>
+
+        <div className="row skill">
+          <div className="three columns header-col">
+            <h1>
+              <span>Skills</span>
+            </h1>
+          </div>
+
+          <div className="nine columns main-col">
+            <p>{skillmessage}</p>
+
+            <div className="bars">
+              <ul className="skills">{skills}</ul>
+            </div>
+          </div>
+        </div>
       </section>
     );
   }
