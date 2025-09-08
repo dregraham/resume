96c1eb228541295c3c6ff821d4e89bd42e7a8fe7
Dre Graham <dre.graham@auctane.com>
2025-09-08 01:05:33 +0000

    remove color-voting-api

--- color-voting-api/src/index.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import "./styles.css";
-import App from "./App";
-
-const rootEl = document.getElementById("root");
-createRoot(rootEl).render(<App />);
