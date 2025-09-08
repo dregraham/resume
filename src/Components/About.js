1221865c82379c96b9a258800443fb2db5cd8e59
Dre Graham <dre.graham@auctane.com>
2025-09-08 16:02:28 +0000

    Added Resume Download pdf

--- src/Components/About.js
+++ src/Components/About.js
@@ -45,7 +45,7 @@ class About extends Component {
                 </div>
                 <div className="columns download">
                   <p>
-                    <a href={resumeDownload} className="button">
+                    <a href={resumeDownload} className="button" download>
                       <i className="fa fa-download"></i>Download My Resume
                     </a>
                   </p>
