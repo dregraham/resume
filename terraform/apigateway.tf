ce6705f54ba4c720f281639c459ad20e9eaac6ff
dregraham <andre.graham709@gmail.com>
2025-11-14 12:12:38 -0600

    create functions removed

--- terraform/apigateway.tf
+++ terraform/apigateway.tf
@@ -11,32 +11,8 @@ locals {
   base_invoke_url        = "https://1c5u47evyg.execute-api.us-east-2.amazonaws.com/prod"
 }
 
-###############################################################################
-# Integration & route (will create if not present; import first if they exist)
-###############################################################################
-resource "aws_apigatewayv2_integration" "dispatch" {
-  api_id                 = local.dispatch_api_id
-  integration_type       = "AWS_PROXY"
-  integration_method     = "POST"
-  integration_uri        = aws_lambda_function.dispatch.invoke_arn
-  payload_format_version = "2.0"
-}
-
-resource "aws_apigatewayv2_route" "dispatch_post" {
-  api_id    = local.dispatch_api_id
-  route_key = "POST /terraform"
-  target    = "integrations/${aws_apigatewayv2_integration.dispatch.id}"
-}
-
-###############################################################################
-# Lambda permission (always ensure invocation allowed)
-###############################################################################
-resource "aws_lambda_permission" "dispatch_invoke" {
-  statement_id  = "AllowAPIGatewayInvoke"
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.dispatch.function_name
-  principal     = "apigateway.amazonaws.com"
-  source_arn    = "${local.dispatch_execution_arn}/*/*"
+data "aws_apigatewayv2_api" "dispatch" {
+  api_id = local.dispatch_api_id
 }
 
 output "dispatch_api_url" {
--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -19,30 +19,14 @@ data "aws_iam_policy_document" "lambda_assume" {
   }
 }
 
-
-
 data "aws_iam_role" "terraform_execution_role" {
   name = "TerraformExecutionRole"
 }
 
-resource "aws_lambda_function" "dispatch" {
-  function_name    = "terraform-dispatch-lambda"
-  handler          = "index.handler"
-  runtime          = "nodejs18.x"
-  role             = data.aws_iam_role.terraform_execution_role.arn
-  filename         = data.archive_file.dispatch.output_path
-  source_code_hash = data.archive_file.dispatch.output_base64sha256
-
-  environment {
-    variables = {
-      GITHUB_WORKFLOW_TOKEN = var.github_token
-      GITHUB_OWNER          = var.github_owner
-      GITHUB_REPO           = var.github_repo
-      DEFAULT_REGION        = var.default_region
-      CORS_ALLOW_ORIGIN     = var.cors_allow_origin
-    }
-  }
-}
 data "aws_lambda_function" "dispatch" {
   function_name = "terraform-dispatch-lambda"
+}
+
+data "aws_iam_policy" "lambda_access" {
+  name        = "TerraformLambdaAccess"
 }
