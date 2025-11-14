cea277eff170c6d279f31a51ea87b36a91d59983
dregraham <andre.graham709@gmail.com>
2025-11-14 09:54:48 -0600

    updates to lambda role referenced

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -43,11 +43,6 @@ resource "aws_lambda_function" "dispatch" {
     }
   }
 }
-
-data "aws_iam_role" "lambda_exec" {
-  name = "terraform-dispatch-lambda-exec"
-}
-
 data "aws_lambda_function" "dispatch" {
   function_name = "terraform-dispatch-lambda"
 }
