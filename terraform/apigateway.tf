9ee5ecf1b97ac83199543fb50329b7eee4079cac
dregraham <andre.graham709@gmail.com>
2025-11-14 16:11:51 -0600

    changes

--- terraform/apigateway.tf
+++ terraform/apigateway.tf
@@ -1,9 +1,5 @@
 ###############################################################################
-# API Gateway (existing via var.existing_api_id)
-###############################################################################
-
-###############################################################################
-# Locals for shared API Gateway
+# API Gateway Configuration with CORS Support
 ###############################################################################
 locals {
   dispatch_api_id        = "1c5u47evyg"
@@ -13,6 +9,22 @@ locals {
 
 data "aws_apigatewayv2_api" "dispatch" {
   api_id = local.dispatch_api_id
+}
+
+# Reference existing routes and integration
+data "aws_apigatewayv2_route" "dispatch_options" {
+  api_id   = local.dispatch_api_id
+  route_id = "v9k66ev"
+}
+
+data "aws_apigatewayv2_route" "dispatch_post" {
+  api_id   = local.dispatch_api_id
+  route_id = "zht2bvl"
+}
+
+data "aws_apigatewayv2_integration" "dispatch_lambda" {
+  api_id        = local.dispatch_api_id
+  integration_id = "hqdgn90"
 }
 
 output "dispatch_api_url" {
