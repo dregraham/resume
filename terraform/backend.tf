b838813a699f5376eac03a561afa37bf85ac83ec
dregraham <andre.graham709@gmail.com>
2025-11-19 13:14:25 -0600

    backend.tf updates

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket         = "dre-multicloud-demo-site"
-    key            = "multicloud-iac/terraform.tfstate"
+    bucket         = "dres-terraform-state-bucket"
+    key            = "shared/terraform.tfstate"
     region         = "us-east-2"
     encrypt        = true
     dynamodb_table = "terraform-state-locks"
