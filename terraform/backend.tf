911d92dbf731df01597de9b05f3d491cde485dfc
dregraham <andre.graham709@gmail.com>
2025-11-19 11:49:48 -0600

    added .tf state table

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -4,5 +4,6 @@ terraform {
     key            = "multicloud-iac/terraform.tfstate"
     region         = "us-east-2"
     encrypt        = true
+    dynamodb_table = "terraform-state-locks"
   }
 }
--- terraform/dynamodb.tf
+++ terraform/dynamodb.tf
@@ -1,3 +1,7 @@
 data "aws_dynamodb_table" "multicloud_runs" {
   name = "multicloud_iac_runs"
 }
+
+data "aws_dynamodb_table" "terraform_state_locks" {
+  name = "terraform-state-locks"
+}
