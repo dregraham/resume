c24b5803ebfa1626afd74023abe3c7ed1f7fa713
dregraham <andre.graham709@gmail.com>
2025-11-22 13:47:21 -0600

    added .jar to public folder

--- src/pages/FlexiblePaymentSchedule/FlexiblePaymentSchedule.js
+++ src/pages/FlexiblePaymentSchedule/FlexiblePaymentSchedule.js
@@ -232,7 +232,7 @@ const FlexiblePaymentSchedule = () => {
             </div>
             <div className="space-y-4">
               <a
-                href="/src/pages/FlexiblePaymentSchedule/target/payment-calculator-1.0.0.jar"
+                href="/payment-calculator-1.0.0.jar"
                 download="payment-calculator-1.0.0.jar"
                 className="block w-full bg-blue-600 text-white px-6 py-4 rounded-md hover:bg-blue-700 transition-colors text-center font-medium"
               >
